723fca09d2f55605b8b4df3a2307137803f094c2
ifpr <guilhermefrandina@gmail.com>
2023-04-27 12:43:31 -0300

    botei mais

---
+++ app/src/main/java/com/example/dasda/jogo.java
@@ -0,0 +1,14 @@
+package com.example.dasda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
